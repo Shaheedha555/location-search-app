4c2db513a9a3009dc7fb6a3e7b100b54730585a0
Shaheedha <shaheedhamolshahi@gmail.com>
2023-03-10 13:52:30 +0530

    last commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const router = createRouter({ history: createWebHistory() }, [
+  { path: "/", component: App },
+]);
+
+app.use(router);
+app.mount("#app");
